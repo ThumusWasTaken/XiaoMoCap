d2834ab50554ee91fbba297529134c4d55669a83
ilanatol <ilanatol@gmail.com>
2025-07-15 01:07:52 +0100

    Add files via upload

---
+++ W25N01GV_LED/Logger.h
@@ -0,0 +1,27 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include "W25N01GV.h"
+#include "BadBlockManager.h"
+
+class Logger {
+public:
+    Logger(W25N01GV& flash, BadBlockManager& badBlockManager);
+    
+    bool begin();
+    void logRecord(const String& record);
+    void flush();
+    void readAllLogs();
+    uint16_t getCurrentPage() { return _currentPage; }
+
+
+private:
+    W25N01GV& _flash;
+    BadBlockManager& _bbm;
+    uint8_t _buffer[2048];
+    uint16_t _bufferPos;
+    uint16_t _currentPage;
+    uint16_t _currentBlock;
+};
+
+#endif
